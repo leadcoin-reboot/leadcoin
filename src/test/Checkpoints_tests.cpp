0614f206d8b07e604f4cdcbcd80c03aefb8bc014
leadcoin-reboot <exd@mail.ru>
2014-04-10 00:00:43 +0400

    New splashscreens

--- src/test/Checkpoints_tests.cpp
+++ src/test/Checkpoints_tests.cpp
@@ -14,21 +14,21 @@ BOOST_AUTO_TEST_SUITE(Checkpoints_tests)
 
 BOOST_AUTO_TEST_CASE(sanity)
 {
-    uint256 p1500 = uint256("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967");
-    uint256 p120000 = uint256("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131");
-    BOOST_CHECK(Checkpoints::CheckBlock(1500, p1500));
-    BOOST_CHECK(Checkpoints::CheckBlock(120000, p120000));
+    uint256 p1912 = uint256("0xfd2e3dd687bbe08854cbdb166515e4cca7705a5f3d255ca492701373a44fcaf8");
+    uint256 p252690 = uint256("0x312ae083d12b9cb7879da4013bce73763de4c1403789f5459fab1c8170f18b74");
+    BOOST_CHECK(Checkpoints::CheckBlock(1912, p1912));
+    BOOST_CHECK(Checkpoints::CheckBlock(252690, p252690));
 
-    
+
     // Wrong hashes at checkpoints should fail:
-    BOOST_CHECK(!Checkpoints::CheckBlock(1500, p120000));
-    BOOST_CHECK(!Checkpoints::CheckBlock(120000, p1500));
+    BOOST_CHECK(!Checkpoints::CheckBlock(1912, p252690));
+    BOOST_CHECK(!Checkpoints::CheckBlock(252690, p1912));
 
     // ... but any hash not at a checkpoint should succeed:
-    BOOST_CHECK(Checkpoints::CheckBlock(1500+1, p120000));
-    BOOST_CHECK(Checkpoints::CheckBlock(120000+1, p1500));
+    BOOST_CHECK(Checkpoints::CheckBlock(1912+1, p252690));
+    BOOST_CHECK(Checkpoints::CheckBlock(252690+1, p1912));
 
-    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 120000);
-}    
+    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 252690);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
