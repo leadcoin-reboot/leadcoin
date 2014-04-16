ed12c90c68d08547e49de8a9d8fa93338655601e
leadcoin-reboot <exd@mail.ru>
2014-04-16 17:42:08 +0400

    Ditch old clients, staticly link openssl in linux builds

--- src/version.h
+++ src/version.h
@@ -35,11 +35,11 @@ static const int MIN_PEER_PROTO_VERSION = 70002;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 70002;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 32400;
+static const int NOBLKS_VERSION_END = 70001;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
